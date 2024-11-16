76b4c98b260aaa4175f67d3d4bc0036623073f40
Archi Jham <117108523+archi101@users.noreply.github.com>
2024-11-16 10:38:51 +0530

    Update firebase.js

--- src/assets/Components/Special60Component/firebase.tsx
+++ src/assets/Components/Special60Component/firebase.tsx
@@ -3,13 +3,13 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDnUTeW0RyOJoLw6BC80ydZr1rChhWF0Dk",
+  apiKey: "",
   authDomain: "codeeditor-a8a0e.firebaseapp.com",
   projectId: "codeeditor-a8a0e",
   storageBucket: "codeeditor-a8a0e.firebasestorage.app",
-  messagingSenderId: "823190944132",
-  appId: "1:823190944132:web:4a0c504b7a9450aaf2c5d1",
-  measurementId: "G-EDRR782EZS"
+  messagingSenderId: "",
+  appId: "",
+  measurementId: ""
 };
 
 // Initialize Firebase
